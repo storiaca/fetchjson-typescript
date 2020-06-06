879bc442f307c9ae3acbe9260a3a48c3aafa1fd6
Aleksandar Ristic <storiaca@gmail.com>
2020-06-06 12:57:55 +0200

    Lesson 5: One quick change

--- index.ts
+++ index.ts
@@ -3,5 +3,17 @@ import axios from "axios";
 const url = "https://jsonplaceholder.typicode.com/todos/1";
 
 axios.get(url).then((response) => {
-  console.log(response.data);
+  const todo = response.data;
+
+  // const id = todo.id;
+  // const title = todo.title;
+  // const completed = title.completed;
+
+  const { id, Title, completed } = todo;
+
+  console.log(`
+  The Todo with ID: ${id}
+  Has a title of: ${Title}
+  Is it finished? ${completed}
+  `);
 });
